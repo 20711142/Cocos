c78dcd2a71908d3da3ba651dfa397aa876f3ffe3
HIMANSHU SAXENA 20711142 <999himanshusaxena@gmail.committed>
2022-02-22 19:30:35 +0530

    Test of Week-1

---
+++ src/main/java/Concat/test/coco.java
@@ -0,0 +1,13 @@
+package Concat.test;
+
+public class coco 
+{
+
+	public static void main(String[] args) 
+	{
+		String s1="himanshu";
+		String s2="Saxena";
+		System.out.print("Concatinated is : ");
+		System.out.println("Hello "+s1.concat(s2));
+	}
+}
